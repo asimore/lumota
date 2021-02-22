a951723a9feb9a138e71182c53b99f54a727770d
asimore <asim.ranjha@gmail.com>
2021-02-22 21:30:26 +0000

    handle bytes to string

--- luminotaservice.py
+++ luminotaservice.py
@@ -7,16 +7,18 @@ def check_version():
 
     os.close(fd)
 
-    return readBytes
+    return readBytes.decode('utf-8')
 
 if __name__ == '__main__':
 
-	os.system('sh /home/pi/projects/lumin/Lumin_FW_Src/audio_application/python/lumingit.sh')
-	model_name = check_version()
+	#os.system('sh /home/pi/projects/lumin/Lumin_FW_Src/audio_application/python/lumingit.sh')
+        model_name = check_version()
         print (model_name)
-	if model_name.upper().strip() == 'NO_UPDATES':
+        if model_name.upper().strip() == 'NO_UPDATES':
            print ('no updates found.')
         else:
            os.system('sh /home/pi/projects/lumin/Lumin_FW_Src/audio_application/python/luminota.sh ' + model_name)
-	
+           os.system('service luminled stop')
+           os.system('cp ./LED_control.py /home/pi/projects/lumin/Lumin_FW_Src')
+           os.system('service luminled start')
 exit(0)
